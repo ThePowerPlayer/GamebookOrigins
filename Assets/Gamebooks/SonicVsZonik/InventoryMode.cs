ccbd529d906f07a32ba0a517144578de3846ea2f
ThePowerPlayer <thepowerplayer452@gmail.com>
2024-04-17 12:28:49 -0400

    Make inventory mode work with menu screen

--- Assets/Gamebooks/SonicVsZonik/InventoryMode.cs
+++ Assets/Gamebooks/SonicVsZonik/InventoryMode.cs
@@ -1,21 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InventoryMode : MonoBehaviour
 {
 	public static bool inventoryMode;
 	public GameObject[] GameplayObjects;
 	public GameObject[] InventoryObjects;
+	public SonicVsZonikMenu SVZMenu;
+	private Scene scene;
+	
+	void Start() {
+		scene = SceneManager.GetActiveScene();
+	}
 	
 	public void ToggleInventoryMode() {
 		inventoryMode = !inventoryMode;
 		Debug.Log("Inventory mode is " + inventoryMode);
-		foreach(GameObject obj in GameplayObjects) {
+		foreach (GameObject obj in GameplayObjects) {
 			obj.SetActive(!inventoryMode);
 		}
-		foreach(GameObject obj in InventoryObjects) {
+		foreach (GameObject obj in InventoryObjects) {
 			obj.SetActive(inventoryMode);
+		}
+		
+		if (scene.name == "SonicVsZonikMenu" && !inventoryMode) {
+			SVZMenu.ChangeButtons();
 		}
 	}
 }
