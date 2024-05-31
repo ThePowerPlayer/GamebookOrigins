6812fd3155f1c177bde6fed0951c1b55291c3f39
ThePowerPlayer <thepowerplayer452@gmail.com>
2024-05-31 01:27:09 -0400

    Add more music tracks

--- Assets/Gamebooks/SonicVsZonik/Scripts/SonicVsZonikMusicManager.cs
+++ Assets/Gamebooks/SonicVsZonik/Scripts/SonicVsZonikMusicManager.cs
@@ -10,6 +10,22 @@ public class SonicVsZonikMusicManager : MonoBehaviour
 	public AudioClip GreenHill;
 	public AudioClip TailsIsHurt;
 	public AudioClip ChemicalPlant;
+	public AudioClip MackMadness;
+	public AudioClip AquaticRuin;
+	public AudioClip ChopChopFight;
+	public AudioClip InTheWoods;
+	public AudioClip RobotnikCloud;
+	public AudioClip CasinoNight;
+	public AudioClip PinballHall;
+	public AudioClip PinballMachine;
+	public AudioClip PinballEnding;
+	public AudioClip HillTop;
+	public AudioClip MysticCave;
+	public AudioClip Metropolis;
+	public AudioClip SkyChase;
+	public AudioClip FinalShowdown;
+	public AudioClip GameComplete;
+	
 	
 	// Dictionary: Input is a section number, output is its corresponding song
 	// TODO: Update this to include every section
@@ -19,6 +35,21 @@ public class SonicVsZonikMusicManager : MonoBehaviour
 		Song[1] = GreenHill;
 		Song[200] = TailsIsHurt;
 		Song[201] = ChemicalPlant;
+		Song[46] = MackMadness;
+		Song[42] = AquaticRuin;
+		Song[69] = ChopChopFight;
+		Song[128] = InTheWoods;
+		Song[28] = RobotnikCloud;
+		Song[65] = CasinoNight;
+		Song[66] = PinballHall;
+		Song[274] = PinballMachine;
+		Song[45] = PinballEnding;
+		Song[82] = HillTop;
+		Song[121] = MysticCave;
+		Song[191] = Metropolis;
+		Song[269] = SkyChase;
+		Song[214] = FinalShowdown;
+		Song[300] = GameComplete;
 	}
 	
 	public void PlaySongForSection(int section) {
